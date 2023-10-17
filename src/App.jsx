f1829a2b66ac853397bdf44437d58793162e6535
Jordan Lee <jtlee@rmgj2.com>
2023-10-17 16:03:18 -0500

    Functionality working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import CodeGeneration from "./CodeGeneration.jsx";
-import Update from "./CodeOutput";
+
 
 function App() {
-  // TODO: Add a dropdown to change the language
-  // TODO: Change the layout
   // TODO: Add a button to copy the code
-  // TODO: Add box for output with syntax highlighting
 
   return (
     <>
@@ -32,27 +29,29 @@ function App() {
             <select
               className="form-select language-select"
               aria-label="Default select example"
+              id="language-select"
             >
               <option value="default">Select Language</option>
-              <option value="python">Python</option>
-              <option value="javascript">Javascript</option>
-              <option value="java">Java</option>
-              <option value="c">C</option>
-              <option value="c++">C++</option>
-              <option value="c#">C#</option>
+              <option value="Python">Python</option>
+              <option value="Javascript">Javascript</option>
+              <option value="Java">Java</option>
+              <option value="C">C</option>
+              <option value="C++">C++</option>
+              <option value="C#">C#</option>
             </select>
-            <button className="btn btn-primary mt-5" onClick={CodeGeneration}>
-              Convert to C++
+            <button className="btn btn-primary mt-5" onClick={CodeGeneration} id='convert'>
+              Convert
             </button>
           </div>
           <div className="col">
             <div className="code-output"></div>
-                <textarea
-                  disabled
-                  id="output-text"
-                  className=""
-                  placeholder="Output code here"
-                ></textarea>
+            <textarea
+              disabled
+              id="preview"
+              className=""
+              placeholder="Output code here"
+            ></textarea>
+
           </div>
         </div>
       </div>
--- src/CodeGeneration.jsx
+++ src/CodeGeneration.jsx
@@ -1,29 +1,41 @@
-import OpenAI from 'openai';
-import prism from 'prismjs';
+import OpenAI from "openai";
 
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_API_KEY,
   dangerouslyAllowBrowser: true,
 });
-function TakeInput(){
-
-  return input
+function TakeInput() {
+  return input;
 }
 
-async function CodeGeneration(){
-  const output = document.getElementById("output-text");
+async function CodeGeneration() {
+  const output = document.getElementById("preview");
   const input = document.getElementById("user-text").value;
+  const language = document.getElementById("language-select").value;
+  const button = document.getElementById("convert");
+  button.disabled = true;
+  button.innerHTML = "Converting...";
   console.log(input);
-  const chatCompletion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [{"role": "user", "content": `Convert this code to C++`},
-               {"role": "user", "content": input}],
-  });
-  output.innerHTML = chatCompletion.choices[0].message.content
-  console.log(chatCompletion.choices[0].message.content);
+  if (language === "default") {
+    alert("Please select a language");
+    return;
+  } else {
+    const chatCompletion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Convert this code to ${language}`,
+        },
+        { role: "user", content: `${input}` },
+        { role: "user", content: "Output as plaintext" },
+      ],
+    });
+    output.innerHTML = chatCompletion.choices[0].message.content;
+    console.log(chatCompletion.choices[0].message.content);
+    button.innerHTML = `Convert`;
+    button.disabled = false;
+  }
 }
 
-
-
-
-export default CodeGeneration
+export default CodeGeneration;
